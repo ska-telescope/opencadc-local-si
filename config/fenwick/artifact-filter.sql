a36fffa2cea4d174b3ce99af57fb05d78bcb4539
Franz Kirsten @ cirrus100 <franz.kirsten@chalmers.se>
2023-04-26 20:00:46 +0200

    added data distribution plan for Swedish site

--- config/fenwick/artifact-filter.sql
+++ config/fenwick/artifact-filter.sql
@@ -3,3 +3,12 @@
 # MUST begin with the WHERE keyword.
 # ex:
 # WHERE uri LIKE '%SOME CONDITION%'
+where split_part(uri,'/',1) in (
+    'cadc:VGPS',
+    'cadc:VLASS',
+    'cadc:WALLABY',
+    'casda:RACS'
+) or (
+    split_part(uri,'/',1) = 'nrao:VLASS'
+    and (uriBucket between '00000' and '55555' or uriBucket between 'aaaab' and 'fffff')
+)
