68e7a1a0dbadc00d14039ca1296ab1a76776ee9f
Jesus Sanchez <jsanchez@iaa.es>
2023-06-28 08:02:14 +0000

    Update file spsrc-si-fresh-restart.sh

--- spsrc-si-fresh-restart.sh
+++ spsrc-si-fresh-restart.sh
@@ -6,18 +6,18 @@
 CONFIG_FOLDER="/home/cloud-user/si/config"
 LOGS_FOLDER="/home/cloud-user/si/logs"
 # Define where the data will live, directory need XXX permissons
-DATA_DIR="/data"
+DATA_DIR="/mnt/scratch/"
 SI_REPO_PATH="images.opencadc.org/storage-inventory"
-MINOC_VERSION=0.9.1
+MINOC_VERSION=0.9.6
 TANTAR_VERSION=0.4.0
 RATIK_VERSION=0.1.4
 FENWICK_VERSION=0.5.5
 CRITWALL_VERSION=0.4.3
-LUSKAN_VERSION=0.6.0
+LUSKAN_VERSION=0.6.4
 POSTGRESQL_IMAGE="swsrc/cadc-postgresql-12-dev"
 HAPROXY_IMAGE="amigahub/cadc-haproxy"
-WAIT_SECONDS=7
-WAIT_SECONDS_LONG=7
+WAIT_SECONDS=15
+WAIT_SECONDS_LONG=60
 HAPROXY_EXPOSED_PORT=443
 
 docker pull ${POSTGRESQL_IMAGE}
@@ -32,7 +32,9 @@ docker pull ${SI_REPO_PATH}/luskan:${LUSKAN_VERSION}
 # Instantiate cadc-postgresql
 docker run -d --volume=${CONFIG_FOLDER}/config/postgresql:/config:ro \
               --volume=${LOGS_FOLDER}/:/logs:rw \
-              -p 5432:5432 
+              --volume=${CONFIG_FOLDER}/dbdata:/var/lib/pgsql/12/data:rw \
+              --network=sinet \
+              -p 5432:5432 \
               --name pg12db 
               ${POSTGRESQL_IMAGE}:latest
 
@@ -44,6 +46,7 @@ docker run -d --user tomcat:tomcat  --link pg12db:pg12db \
        --volume=${CONFIG_FOLDER}/config/minoc:/config:ro \
        --volume=${DATA_DIR}:/data:rw \
        --name minoc \
+       --network=sinet \
        ${SI_REPO_PATH}/minoc:${MINOC_VERSION}
 
 #Wait
@@ -53,23 +56,28 @@ sleep ${WAIT_SECONDS}
 docker run -d --user tomcat:tomcat  --link pg12db:pg12db \
        --volume=${CONFIG_FOLDER}/config/luskan:/config:ro \
        --name luskan \
+       --network=sinet\
        ${SI_REPO_PATH}/luskan:${LUSKAN_VERSION}
       # optionally add the below if baseStorageDir in cadc-tap-tmp.properties is not /tmp
       #--volume=/dir/on/host:/data:rw
 
 #Wait 
 sleep ${WAIT_SECONDS}
+
 # Instantiate fenwick
-docker run -d --user opencadc:opencadc  --link pg10db:pg10db \
-              --volume=${CONFIG_FOLDER}/config/fenwick:/config:ro 
-              --name fenwick 
+docker run -d --user opencadc:opencadc  --link pg12db:pg12db \
+              --volume=${CONFIG_FOLDER}/config/fenwick:/config:ro \
+              --name fenwick \
+              --network=sinet \
               ${SI_REPO_PATH}/fenwick:${FENWICK_VERSION}
 #Wait 
 sleep ${WAIT_SECONDS}
+
 # Instantiate critwall
-docker run -d --user opencadc:opencadc  --link pg10db:pg10db \
-              --volume=${CONFIG_FOLDER}/config/critwall:/config:ro 
-              --name critwall 
+docker run -d --user opencadc:opencadc  --link pg12db:pg12db \
+              --volume=${CONFIG_FOLDER}/config/critwall:/config:ro \
+              --name critwall \
+              --network=sinet \
               ${SI_REPO_PATH}/critwall:${CRITWALL_VERSION}
 
 sleep ${WAIT_SECONDS}	      
@@ -79,6 +87,7 @@ docker run -d --user opencadc:opencadc  --link pg12db:pg12db \
        --volume=${CONFIG_FOLDER}/config/tantar:/config:ro \
        --volume=${DATA_DIR}:/data:rw \
        --name tantar \
+       --network=sinet \
        ${SI_REPO_PATH}/tantar:${TANTAR_VERSION}
 
 #Wait
@@ -88,6 +97,7 @@ sleep ${WAIT_SECONDS}
 docker run -d --user opencadc:opencadc  --link pg12db:pg12db \
        --volume=${CONFIG_FOLDER}/config/ratik:/config:ro \
        --name ratik \
+       --network=sinet \
        ${SI_REPO_PATH}/ratik:${RATIK_VERSION}
 
 #Wait
@@ -97,10 +107,11 @@ sleep ${WAIT_SECONDS}
 # Instantiate cadc-haproxy
 docker run -d --volume=${LOGS_FOLDER}:/logs:rw \
               --volume=${CERTIFICATES_FOLDER}:/config:ro \
-	      --volume=${CONFIG_FOLDER}/config/haproxy:/usr/local/etc/haproxy/:rw \
-              --link minoc:minoc \
-              --link luskan:luskan \
+	       --volume=${CONFIG_FOLDER}/config/haproxy:/usr/local/etc/haproxy/:rw \
+              #--link minoc:minoc \
+              #--link luskan:luskan \
               -p ${HAPROXY_EXPOSED_PORT}:443 \
               --name haproxy \
+              --network=sinet \
               ${HAPROXY_IMAGE}:latest
 
